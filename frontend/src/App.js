44893e8b8a1f47df402548f5891af97a3d071c4e
TengkuHaikal <22001741@siswa.um.edu.my>
2025-05-08 19:59:56 +0800

    modified max-width to iPad width

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,54 @@
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import About from "./Landing/About";
 import Connect from "./Landing/Connect";
 import Features from "./Landing/Features";
 import Home from "./Landing/Home";
 import SignIn from "./Auth/SignIn";
 import SignUp from "./Auth/SignUp";
-import NavBar from "./components/NavBar"; // Navbar component for navigation
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Dashboard from "./Dashboard/Dashboard";
 
-// const AppWrapper = () => {
-//   const location = useLocation();
-//   const hideNavbarRoutes = ["/login", "/register"]; // Add '/register' if you make one
+const AppWrapper = () => {
+  const location = useLocation();
 
-//   const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+  const hideNavbarRoutes = ["/login", "/register"];
+  const hideSidebarRoutes = ["/", "/login", "/register"];
 
-//   return (
-//     <div className="App">
-//       {!shouldHideNavbar && <NavBar />}
-//       <div className="main-content">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/home" element={<Home />} />
-//           <Route path="/about" element={<About />} />
-//           <Route path="/connect" element={<Connect />} />
-//           <Route path="/features" element={<Features />} />
-//           <Route path="/login" element={<SignIn />} />
-//           <Route path="/register" element={<SignUp />} />
-//         </Routes>
-//       </div>
-//       <Footer /> {/* Footer added here */}
-//     </div>
-//   );
-// };
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+  const shouldHideSidebar = hideSidebarRoutes.includes(location.pathname);
+
+  return (
+    <div className="App">
+      {!shouldHideNavbar && <NavBar />}
+      <div className="main-content" style={{ display: "flex" }}>
+        {!shouldHideSidebar && <SideBar />}
+        <div className="page-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/connect" element={<Connect />} />
+            <Route path="/features" element={<Features />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    // <Router>
-    //   <AppWrapper />
-    // </Router>
-    <Dashboard />
+    <Router>
+      <AppWrapper />
+    </Router>
   );
 };
 
